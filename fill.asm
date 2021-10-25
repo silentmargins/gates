3e1dd6976294ff62b238ed40f30ce8f981834197
Silentmargins <silentmargins@gmail.com>
2021-10-25 12:55:05 +0300

    Update fill.asm

--- fill.asm
+++ fill.asm
@@ -1,12 +1,12 @@
 (START)
 
-@SCREEN
-D=A
+@SCREEN 
+D=A		//D=16384
 @R0
-M=D //save the placeholder variable of screen
+M=D 	//save the placeholder variable of screen, M[0] = 16384
 
 @KBD
-D=M		//check keyboard memory value
+D=M		//check keyboard memory value, D=24576
 @BLACK
 D;JGT		//jumpt to black intruction memory address if contains 1
 @OFF
@@ -15,16 +15,15 @@ D;JEQ		//jump to clear screen instruction block if all are zeroes
 
 (BLACK)
 @R0
-M = -1 		// set the placeholder value to -1 (1111..11)
-
-A = M		//get the address memory placeholder
-D = A + 1		//increment it
+A = M		//get the address memory placeholder, 16384
+M = -1			//FILL IT, set the placeholder value to -1 (1111..11), M[16384] = -1
+D = A + 1		//increment it, D = 16385
 
 @R0
-M = D		//set placeholder to new incremented value
+M = D		//set placeholder to new incremented value, M[0] = 16385
 
 @KBD
-D = M - D	//calculate the remainder
+D = M - D	//calculate the remainder, 24576 - 16385
 
 @BLACK
 D;JGT		//if the remainder is still greater than 0, continue loop
@@ -35,16 +34,15 @@ D;JGT		//if the remainder is still greater than 0, continue loop
 // white block
 (OFF)
 @R0
-M = 0		// set the placeholder value to 0 (00000...00)
-
-A = M		//get the address memory placeholder
-D = A + 1		//increment it
+A = M		//get the address memory placeholder, 16384
+M = 0			//FILL IT, set the placeholder value to 0, M[16384] = 0
+D = A + 1		//increment it, D = 16385
 
 @R0
-M = D		//set placeholder to new incremented value
+M = D		//set placeholder to new incremented value, M[0] = 16385
 
 @KBD
-D = M - D
+D = M - D	//calculate the remainder, 24576 - 16385
 
 @OFF
 D;JGT
