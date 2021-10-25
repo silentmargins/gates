84fbc126d72e35217080308bb4d1166c95212794
Silentmargins <silentmargins@gmail.com>
2021-10-25 17:28:51 +0000

    Update fill.asm

--- fill.asm
+++ fill.asm
@@ -1,3 +1,17 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Fill.asm
+
+// Runs an infinite loop that listens to the keyboard input.
+// When a key is pressed (any key), the program blackens the screen,
+// i.e. writes "black" in every pixel;
+// the screen should remain fully black as long as the key is pressed. 
+// When no key is pressed, the program clears the screen, i.e. writes
+// "white" in every pixel;
+// the screen should remain fully clear as long as no key is pressed.
+
+// Put your code here.
 (START)
 
 @SCREEN 
@@ -14,37 +28,38 @@ D;JEQ		//jump to clear screen instruction block if all are zeroes
 
 
 (BLACK)
-@R0
-A = M		//get the address memory placeholder, 16384
-M = -1			//FILL IT, set the placeholder value to -1 (1111..11), M[16384] = -1
-D = A + 1		//increment it, D = 16385
+@R1 //temp value to store color
+M = -1		
 
-@R0
-M = D		//set placeholder to new incremented value, M[0] = 16385
-
-@KBD
-D = M - D	//calculate the remainder, 24576 - 16385
-
-@BLACK
-D;JGT		//if the remainder is still greater than 0, continue loop
-
-
+@FIXCOLOR
+0;JMP		//if the remainder is still greater than 0, continue loop
 
 
 // white block
 (OFF)
-@R0
-A = M		//get the address memory placeholder, 16384
-M = 0			//FILL IT, set the placeholder value to 0, M[16384] = 0
-D = A + 1		//increment it, D = 16385
+@R1 //temp value to store color
+M = 0		
+
+@FIXCOLOR
+0;JMP		//if the remainder is still greater than 0, continue loop
+
+
+(FIXCOLOR)
+
+@R1
+D = M // GET THE VALUE OF COLOR
 
 @R0
-M = D		//set placeholder to new incremented value, M[0] = 16385
+A = M
+M = D // VALUE OF M[A] = D
+
+@R0
+M = M + 1 //INCREMENT VALUE
 
 @KBD
 D = M - D	//calculate the remainder, 24576 - 16385
 
-@OFF
+@FIXCOLOR
 D;JGT
 
 
